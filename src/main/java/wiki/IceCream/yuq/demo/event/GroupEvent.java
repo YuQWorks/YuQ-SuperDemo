fc49dfcbc3876647d93ff7f0f0346ecdca73ea2c
dc <2586515115@qq.com>
2020-08-24 21:22:20 +0800

    修改

--- src/main/java/wiki/IceCream/yuq/demo/event/GroupEvent.java
+++ src/main/java/wiki/IceCream/yuq/demo/event/GroupEvent.java
@@ -136,6 +136,11 @@ public class GroupEvent {
                     "使用系统前请先下载群文件中的操作手册阅读。\n" +
                     "信息报送网址：http://172.10.8.2:8080/XTBG/frameJsp.do"));
 
+        }else if (event.getGroup().getId() == 1146472411){ // 项目库
+            event.getGroup().sendMessage(new Message().plus("欢迎").plus(mif.at(event.getMember().getId())).plus("加入本群~~\n" +
+                    "请修改群内备注为:市/县-姓名 如XXXX市-张三  \n" +
+                    "项目进度相关问题可在群内提问。"  ));
+
         }else { // 其他
             event.getGroup().sendMessage(new Message().plus("欢迎").plus(mif.at(event.getMember().getId())).plus("加入本群~~\n"));
         }
